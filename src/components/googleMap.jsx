66c0253ab7d38e7c07aede54531cd076676616f0
Matt <maty.britez7@gmail.com>
2023-09-12 22:29:49 -0300

    Google maps script install

---
+++ src/components/googleMap.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+
+const googleMap = () => {
+  return (
+    <div>mapa</div>
+  )
+};
+
+export default googleMap;
