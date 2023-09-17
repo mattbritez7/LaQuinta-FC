b3cc0bab551cf5c95033144670eeea97bbc85d39
Matt <maty.britez7@gmail.com>
2023-09-17 13:54:19 -0300

    Google map in module configurate

--- src/components/googleMap.jsx
+++ src/components/googleMap.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+import GoogleMaps from "simple-react-google-maps";
 
-const googleMap = () => {
+const GoogleMap = () => {
+  const latitud = -34.8483596;
+  const longitud = -58.3573251;
+
   return (
-    <div>mapa</div>
+    <>
+      <div>mapa</div>
+      <GoogleMaps
+        apiKey={"Your Google Api Key"}
+        style={{height: "400px", width: "1000px"}}
+        zoom={6}
+        center={{lat: latitud, lng: longitud}}
+      />
+    </>
   )
 };
 
-export default googleMap;
+export default GoogleMap;
