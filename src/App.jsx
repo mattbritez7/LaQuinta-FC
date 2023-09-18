4b87b2218cd82234f8d95e75da77befaff614d13
Matt <maty.britez7@gmail.com>
2023-09-17 21:42:12 -0300

    Add carrousel for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from './components/Footer.jsx';
 import GoogleMap from './components/GoogleMap.jsx';
+import Carrousel from './components/Carrousel.jsx'
 
 function App() {
   return ( 
     <>
       <h1>La quinta FC</h1>
+      <Carrousel />
       <GoogleMap />
       <Footer />
     </>
