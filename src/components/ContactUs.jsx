160149c0aa2d203cf30a6a214a47a205644234ce
mattbritez7 <maty.britez7@gmail.com>
2023-09-26 13:53:37 -0300

    Mejorate contactUs

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -37,57 +37,17 @@ import {
             borderRadius="lg"
             m={{ sm: 4, md: 16, lg: 10 }}
             p={{ sm: 5, md: 5, lg: 16 }}>
-            <Box p={4}>
+            <Box p={4} textAlign={"center"} mb={"20px"}>
+                <Heading>Contactanos</Heading>
               <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
                 <WrapItem>
-                  <Box>
-                    <Heading>Contact</Heading>
-                    <Text mt={{ sm: 3, md: 3, lg: 5 }} color="gray.500">
-                      Fill up the form below to contact
-                    </Text>
-                    <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
-                      <VStack pl={0} spacing={3} alignItems="flex-start">
-                        <Button
-                          size="md"
-                          height="48px"
-                          width="200px"
-                          variant="ghost"
-                          color="#DCE2FF"
-                          _hover={{ border: '2px solid #1C6FEB' }}
-                          leftIcon={<MdPhone color="#1970F1" size="20px" />}>
-                          +91-988888888
-                        </Button>
-                        <Button
-                          size="md"
-                          height="48px"
-                          width="200px"
-                          variant="ghost"
-                          color="#DCE2FF"
-                          _hover={{ border: '2px solid #1C6FEB' }}
-                          leftIcon={<MdEmail color="#1970F1" size="20px" />}>
-                          hello@abc.com
-                        </Button>
-                        <Button
-                          size="md"
-                          height="48px"
-                          width="200px"
-                          variant="ghost"
-                          color="#DCE2FF"
-                          _hover={{ border: '2px solid #1C6FEB' }}
-                          leftIcon={<MdLocationOn color="#1970F1" size="20px" />}>
-                          Karnavati, India
-                        </Button>
-                      </VStack>
-                    </Box>
-                   
-                  </Box>
                 </WrapItem>
                 <WrapItem>
                   <Box bg="white" borderRadius="lg">
                     <Box m={8} color="#0B0E3F">
                       <VStack spacing={5}>
                         <FormControl id="name">
-                          <FormLabel>Your Name</FormLabel>
+                          <FormLabel>Nombre</FormLabel>
                           <InputGroup borderColor="#E0E1E7">
                             <InputLeftElement pointerEvents="none">
                               <BsPerson color="gray.800" />
@@ -96,7 +56,7 @@ import {
                           </InputGroup>
                         </FormControl>
                         <FormControl id="name">
-                          <FormLabel>Mail</FormLabel>
+                          <FormLabel>Email</FormLabel>
                           <InputGroup borderColor="#E0E1E7">
                             <InputLeftElement pointerEvents="none">
                               <MdOutlineEmail color="gray.800" />
@@ -105,7 +65,7 @@ import {
                           </InputGroup>
                         </FormControl>
                         <FormControl id="name">
-                          <FormLabel>Message</FormLabel>
+                          <FormLabel>Comentario</FormLabel>
                           <Textarea
                             borderColor="gray.300"
                             _hover={{
@@ -114,15 +74,52 @@ import {
                             placeholder="message"
                           />
                         </FormControl>
-                        <FormControl id="name" float="right">
+                        <FormControl id="name" float="none">
                           <Button variant="solid" bg="#0D74FF" color="white" _hover={{}}>
-                            Send Message
+                            Enviar
                           </Button>
                         </FormControl>
                       </VStack>
                     </Box>
                   </Box>
                 </WrapItem>
+                          <Box>
+                              <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
+                                <VStack pl={0} spacing={3} alignItems="flex-start">
+                                  <Button
+                                    size="md"
+                                    height="48px"
+                                    width="200px"
+                                    variant="ghost"
+                                    color="#DCE2FF"
+                                    _hover={{ border: '2px solid #1C6FEB' }}
+                                    leftIcon={<MdEmail color="#1970F1" size="20px" />}>
+                                    info@laquintafc.com.ar
+                                  </Button>
+                                  <Button
+                                    size="md"
+                                    height="48px"
+                                    width="200px"
+                                    variant="ghost"
+                                    color="#DCE2FF"
+                                    _hover={{ border: '2px solid #1C6FEB' }}
+                                    leftIcon={<MdPhone color="#1970F1" size="20px" />}>
+                                    +54 9 11 6228 6543
+                                  </Button>
+                                  <Button
+                                    size="md"
+                                    height="48px"
+                                    width="200px"
+                                    variant="ghost"
+                                    color="#DCE2FF"
+                                    _hover={{ border: '2px solid #1C6FEB' }}
+                                    leftIcon={<MdLocationOn color="#1970F1" size="20px" />}>
+                                    Av. Republica Argentina al 6500, esquina Pedro de Irigoyen
+                                  </Button>
+                                </VStack>
+                              </Box>
+                             
+                        </Box>
               </Wrap>
             </Box>
           <GoogleMap />
