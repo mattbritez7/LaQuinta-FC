9f8cd202bfba7f8b68b7548e473481fd06984dd7
Matt Britez <87097052+mattbritez7@users.noreply.github.com>
2023-10-28 18:09:01 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://www.laquintafc.com.ar"
+  base: 'https://www.laquintafc.com.ar/'
 })
