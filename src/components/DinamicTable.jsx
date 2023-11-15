bb8173f296a014cbb78b0bfbd9469edcfc7326a8
Matt <maty.britez7@gmail.com>
2023-11-14 22:13:40 -0300

    LQFC-21 add styles for dinamictable

--- src/components/DinamicTable.jsx
+++ src/components/DinamicTable.jsx
@@ -1,26 +1,26 @@
-import { TableContainer, Table, Thead, Tr, Th, Tfoot, Tbody, Td} from '@chakra-ui/react'
+import { TableContainer, Table, Thead, Tr, Th, Tfoot, Tbody, Td, Text} from '@chakra-ui/react'
   
 export default function Contact({tableData, boolean}) {
 
     return (
         <>
-        <TableContainer m="25px" border={'1px solid black'}>    
-            <h1>{tableData[0]?.tableName}</h1>
+        <TableContainer p="30px" m="25px" border={'1px solid #00BFFF'} textAlign={'center'} borderTop="4px solid #2e9dff" borderRadius="5px">    
+            <Text fontSize={'18px'} mb={'20px'}><b>{tableData[0]?.tableName}</b></Text>
                 <Table width={'20%'}>
-                    <Thead>
+                    <Thead borderTop="1px solid black"> 
                     <Tr>
-                        <Th><b>Jugador</b></Th>
-                        <Th><b>Equipo</b></Th>
+                        <Th fontSize={'18px'}><b>Jugador</b></Th>
+                        <Th fontSize={'18px'}><b>Equipo</b></Th>
                         {
                             boolean ? 
-                            <Th><b>Razon</b></Th>
+                            <Th fontSize={'18px'}><b>Razon</b></Th>
                             :
-                            <Th><b>Goles</b></Th>
+                            <Th fontSize={'18px'}><b>Goles</b></Th>
                         }
                     </Tr>
                     </Thead>
                 {tableData?.map((data, index) => (
-                    <Tbody key={index}>
+                    <Tbody key={index} color={'#6a859c'}>
                         <Tr>
                             <Td>{data.player}</Td>
                             <Td>{data.club}</Td>
@@ -34,7 +34,6 @@ export default function Contact({tableData, boolean}) {
                     </Tbody>
                 ))}
                 </Table>
-           
             </TableContainer>
         </>
     )
