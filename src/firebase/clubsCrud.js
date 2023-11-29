2a0f94f9aa321e481ead7744df357f0a83b0ef63
Matt <maty.britez7@gmail.com>
2023-11-28 21:48:05 -0300

    add multiple equipos

--- src/firebase/clubsCrud.js
+++ src/firebase/clubsCrud.js
@@ -1,26 +1,24 @@
 import { doc, setDoc } from 'firebase/firestore';
 import { db } from './firebase';
 
-const clubsCrud = async (clubData) => {
+const clubsCrud = async (clubData) => { 
 
-    const rowRange = clubData['!ref'].split(':')[1].match(/\d+/g);
-    // Iterar sobre las filas en el rango y procesar los datos del equipo
-    for (let i = parseInt(rowRange[0]); i <= parseInt(rowRange[1]); i++) {
-      const clubName = clubData[`A${i}`].v;
-      const players = clubData[`B${i}`].v.split(',');
-      console.log(clubName + 'llegue'); 
-
+  for (let i = 1; i < clubData.length; i++) {
+    const club = clubData[i];
+    const clubName = club['A'];
+    const players = club['B'].split(',').map(player => player.trim());
     const clubRef = doc(db, 'clubs1', clubName);
-    console.log(clubName, players);
-    // Utilizar setDoc para agregar o actualizar el documento
-    await setDoc(clubRef, {
-      clubName: clubName,
-      players: players
-      // Otros campos relevantes
-    });
-
-    console.log(`Equipo ${clubName} agregado o actualizado con éxito.`);
+    
+    try {
+      await setDoc(clubRef, {
+        clubName,
+        players
+      });
+      console.log(`Equipo ${clubName} agregado o actualizado con éxito.`);
+    } catch (error) {
+      console.error(`Error al agregar equipo:`, error);
+    }
   }
-}
+};
 
 export default clubsCrud;
--- src/pages/updateStats.jsx
+++ src/pages/updateStats.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react';
 import * as XLSX from 'xlsx';
 import clubsCrud from '../firebase/clubsCrud';
 
 const ExcelUploader = () => {
-  const [file, setFile] = useState([]);
-
+  
   const handleFileChange = (e) => {
     const reader = new FileReader();
     reader.readAsBinaryString(e.target.files[0]);
@@ -15,12 +13,8 @@ const ExcelUploader = () => {
       const sheet = workbook.Sheets[sheetName];
       console.log('hasta aca llegue mostro 1 ' + JSON.stringify(sheet, null, 2));
       const parsedData = XLSX.utils.sheet_to_json(sheet, { header: 'A' });
-      setFile(parsedData);
-
-      console.log('hasta aca llegue mostro 2 ' + selectedFile.name);
-      clubsCrud(sheet);
+      clubsCrud(parsedData);
     }
-    const selectedFile = e.target.files[0];
   };
 
   return (
